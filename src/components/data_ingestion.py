9f394a53d0929272a2a485247c67bedf7005425d
shifapanjwanii <157044609+shifapanjwanii@users.noreply.github.com>
2025-01-30 23:27:51 -0700

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,56 @@
+# main aim is to read the dataset from a specific data source
+# that data source might be created by a Big Data or a Cloud team
+# read data, then split it into train & test
+
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+# some inputs required by the data ingestion component
+# inputs like, where to save train data/test data/raw data
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")   
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
+
+class DataIngestion: 
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info("Train-test split initiated")
+            train_set,test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path, 
+                self.ingestion_config.test_data_path     #returning these so they can be used during data transformation
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
